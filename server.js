70942b841bb22d4fd0f3f401a479e563d0069133
l0stg <61638362+l0stg@users.noreply.github.com>
2024-12-27 21:10:46 +0500

    1

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 8008;
 
 // Раздача статических файлов из папки public
 app.use('/.well-known', express.static('.well-known'));
 
+// Главная страница
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+  
 // Запуск сервера
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
