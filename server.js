1e7bb4fc47aa8c0ce997ec623b0c511781ce2ffc
l0stg <61638362+l0stg@users.noreply.github.com>
2024-12-27 20:16:43 +0500

    1

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Настройка пути для сервера
-app.use('/.well-known', express.static(path.join(__dirname, '.well-known')));
+// Раздача статических файлов из папки public
+app.use('/.well-known', express.static('public/.well-known'));
+
+// Основной маршрут
+app.get('/', (req, res) => {
+    res.send('Hello, your Node.js app is running!');
+});
 
 // Запуск сервера
-const PORT = 8000; // Вы можете использовать любой свободный порт
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}.com`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
