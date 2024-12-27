74bc46ea59ff158d97be5a8cc88e5263ab265dc9
l0stg <61638362+l0stg@users.noreply.github.com>
2024-12-27 18:50:28 +0500

    1

--- js/action_th.js
+++
@@ -1,9 +0,0 @@
-//вот это очень кривая функция для удаления стилей у столбцов, сделано для того чтобы убрать выделение с прошлого столбца
-function action_th(){
-    document.getElementById("thumb_1").className = "title";
-    document.getElementById("thumb_2").className = "title";
-    document.getElementById("thumb_3").className = "title";
-    document.getElementById("thumb_4").className = "title";
-
-
-}
