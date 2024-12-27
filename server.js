dcd85b7e44186d9c996ac47ab5ab2ed8a0876240
l0stg <61638362+l0stg@users.noreply.github.com>
2024-12-27 20:28:28 +0500

    2

--- server.js
+++ server.js
@@ -3,12 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Раздача статических файлов из папки public
-app.use('/.well-known', express.static('public/.well-known'));
-
-// Основной маршрут
-app.get('/', (req, res) => {
-    res.send('Hello, your Node.js app is running!');
-});
+app.use('/.well-known', express.static('.well-known'));
 
 // Запуск сервера
 app.listen(PORT, () => {
